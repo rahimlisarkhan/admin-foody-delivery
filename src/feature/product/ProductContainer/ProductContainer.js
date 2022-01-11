fc604c3e09ebe22efff95a4d9dce4563298914d6
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-01-12 01:55:08 +0400

    update

--- src/feature/product/ProductContainer/ProductContainer.js
+++ src/feature/product/ProductContainer/ProductContainer.js
@@ -2,7 +2,7 @@ import { Fragment } from "react"
 import { useTranslation } from "react-i18next";
 import ContentHeader from "../../../components/ContentHeader"
 import { ProductsList } from "./ProductContainer.styled";
-// import ProductCard from "../ProductCard"
+import ProductCard from "../ProductCard"
 import Paginations from "../../../components/Pagination";
 
 
@@ -13,11 +13,11 @@ export const ProductContainer = () => {
         <Fragment>
             <ContentHeader title={t('products')}/>
             <ProductsList>
-                {/* <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
-                <ProductCard/> */}
+                <ProductCard/>
+                <ProductCard/>
             </ProductsList>
             <Paginations/>
         </Fragment>
