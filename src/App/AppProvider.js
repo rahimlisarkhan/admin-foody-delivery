92724c386e048746c0f154f1c8c66c4a69300b92
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-01-11 22:09:30 +0400

    update

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,16 +1,19 @@
 import {
     BrowserRouter as Router,
-  } from "react-router-dom";
+} from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../style/global";
+
 import { theme } from "../style/theme";
 
 
-  export const AppProvider = ({children}) => {
-      return(
-      <Router>
-        <ThemeProvider theme={theme}>
-            {children}
-        </ThemeProvider >
-      </Router>
-      )
-  }
+export const AppProvider = ({ children }) => {
+    return (
+        <Router>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                {children}
+            </ThemeProvider >
+        </Router>
+    )
+}
