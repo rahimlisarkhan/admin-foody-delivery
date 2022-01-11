67d9a71000dd9b189d193f0e2a0c5021eae9881e
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-01-12 01:51:34 +0400

    update

--- src/feature/product/ProductContainer/ProductContainer.js
+++ src/feature/product/ProductContainer/ProductContainer.js
@@ -2,8 +2,8 @@ import { Fragment } from "react"
 import { useTranslation } from "react-i18next";
 import ContentHeader from "../../../components/ContentHeader"
 import { ProductsList } from "./ProductContainer.styled";
-import ProductCard from "../ProductCard"
-import Paginations from "../../../components/Pagination";
+// import ProductCard from "../ProductCard"
+// import Paginations from "../../../components/Pagination";
 
 
 export const ProductContainer = () => {
@@ -13,13 +13,13 @@ export const ProductContainer = () => {
         <Fragment>
             <ContentHeader title={t('products')}/>
             <ProductsList>
+                {/* <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+                <ProductCard/> */}
             </ProductsList>
-            <Paginations/>
+            {/* <Paginations/> */}
         </Fragment>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import Layout from "../components/Layout";
 import { ROUTE } from "../util/route";
 
 const DashboardPage = React.lazy(() => import('../pages/dashboard'));
-// const ProductsPage = React.lazy(() => import('../pages/products'));
-const Basket = React.lazy(() => import('../components/Basket'));
+const ProductsPage = React.lazy(() => import('../pages/products'));
+// const Basket = React.lazy(() => import('../components/Basket'));
 
 const RootPage = () => {
     return (
         <Layout>
             <Switch>
                 <Route exact path={ROUTE.MAIN.PANEL.CHILD.DASHBOARD.HREF} component={DashboardPage} />
-                <Route exact path={ROUTE.MAIN.PANEL.CHILD.PRODUCTS.HREF} component={Basket} />
+                <Route exact path={ROUTE.MAIN.PANEL.CHILD.PRODUCTS.HREF} component={ProductsPage} />
                 <Redirect exact from={ROUTE.MAIN.PANEL.HREF} to={ROUTE.MAIN.PANEL.CHILD.DASHBOARD.HREF} />
             </Switch>
         </Layout>
